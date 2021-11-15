0e14fc44f728ff5ba13feb6606146fb8c4976500
Budi Sendjaja <bsendjaja@gmail.com>
2021-11-15 10:23:50 -0600

    Fix test semaphore wait process to exit

--- src/test_semaphore.cpp
+++ src/test_semaphore.cpp
@@ -1,29 +1,43 @@
 #include "test_semaphore.h"
 
+void endProcess()
+{
+    printf("exit\n");
+}
+
 int test_semaphore()
 {
     printf("Begin test_semaphore\n");
     sem_t *sem1 = sem_open("test_semaphore", O_CREAT, S_IRUSR | S_IWUSR, 0);
-    // sem_t *sem2 = sem_open("test_semaphore", O_CREAT|O_EXCL, mode, value);
+    sem_t *sem2 = sem_open("test_semaphore", O_CREAT, S_IRUSR | S_IWUSR, 0);
+    int pid = 0;
 
-    if(sem1)
+    if(sem1 && sem2)
     {
-        int pid = fork();
+        // getppid will return either the shell pid or gdb/lldb pid
+        printf("Before fork\t pid:%d ppid:%d\n", getpid(), getppid());
+        pid = fork();
 
         if(pid == 0)
         {
-            printf("child: test1\n");
+            printf("child1\t\t pid:%d ppid:%d\n", getpid(), getppid());
             sem_post(sem1);
-            // exit(0);
-            printf("child: test2\n");
+            printf("child2\n");
+
+            atexit(endProcess);
+            // exit function will call endProcess
+            exit(0);
+
+            // _exit function will not call endProcess
+            // _exit(0);
         }
         else
         {
-            printf("parent: test1\n");
-            // sleep(1);
-            // wait(NULL);
+            int status;
+            printf("parent1\t\t pid:%d ppid:%d\n", getpid(), getppid());
             sem_wait(sem1);
-            printf("parent: test2\n");
+            printf("parent2\n");
+            (void)waitpid(pid, &status, 0);
         }
     }
     else
@@ -31,6 +45,5 @@ int test_semaphore()
         printf("Fail in sem_open errno %d\n", errno);
     }
 
-    // This test cause the next input to the choose test to be ignored
     return 0;
 }
