7e6bf8453f4154e5ba1d49b4c3f93a5cbab20309
Budi Sendjaja <bsendjaja@gmail.com>
2022-01-25 23:11:50 -0600

    Using jq to get JIRA summary

--- release_note/generate.sh
+++ release_note/generate.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# program needed
+# git, awk, sed, uniq
+
+Server="https://jira.atlassian.com/rest/api/2/issue/"
+
+# Need this, but not MINE
+JIRA_TOKEN=
+
+# In case if tickets are moved under NRF-
+JIRA_TYPE="NEUR-"
+
+#DEBUG
+# parameterA="develop"
+# parameterB="IPG-v86.0"
+parameterA="v01.0"
+parameterB="v00.0"
+
 helpFunction()
 {
     echo "Usage: $0 -a parameterA -b parameterB" # -c parameterC"
@@ -19,9 +36,7 @@ do
    esac
 done
 
-#DEBUG
-parameterA="develop"
-parameterB="IPG-v86.0"
+
 
 # Print helpFunction in case parameters are empty
 if [ -z "$parameterA" ] || [ -z "$parameterB" ] # || [ -z "$parameterC" ]
@@ -36,30 +51,46 @@ echo "$parameterB"
 # echo "$parameterC"
 
 # Get the list of merge between parameterA and parameterB
-#git log --pretty=oneline --first-parent $parameterA...$parameterB > commit_list.txt
+git log --pretty=oneline --first-parent $parameterA...$parameterB > commit_list.txt
 
 # Find the first occurence of NEUR- and the following 5 characters
-awk 'match($0, /NEUR-/) {
+awk -v ref=$JIRA_TYPE 'match($0, ref) {
     print substr($0, RSTART, RLENGTH+8)
 }
-' sample_data.txt > commit_list_1.txt
-#' commit_list.txt > commit_list_1.txt
-
+' commit_list.txt > commit_list_1.txt
 
 # Only allow 0-9
 sed 's/[^0-9]*//g' commit_list_1.txt > commit_list_2.txt
 
 # Check the length of each line
-awk '{print length}' commit_list_2.txt | sort -n | uniq -c > commit_list_length.txt
+#awk '{print length}' commit_list_2.txt | sort -n | uniq -c > commit_list_length.txt
 
 # more commit_list.txt| awk -F'NEUR' '{print $2}'
 #sed 's/^NEUR-/NEUR-/' commit_list.txt > commit_list_1.txt
 # Remove the first column of the commit hash
 #sed 's/^ *//' commit_list.txt | cut -d" " -f2- > commit_list_1.txt
 
-
 # Remove all duplicates using unique
 uniq commit_list_2.txt > commit_list_3.txt
 
+awk -v ref=$JIRA_TYPE '{print ref $0}' commit_list_3.txt > commit_list_4.txt
+
 # Sort if needed
-sort commit_list_3.txt > commit_list_sorted.txt
+#sort commit_list_3.txt > commit_list_sorted.txt
+
+rm commit_list_1.txt commit_list_2.txt commit_list.txt commit_list_3.txt
+
+TICKET_LIST=commit_list_sample.txt
+
+IFS=$'\n'       # make newlines the only separator
+set -f          # disable globbing
+for i in $(cat < $TICKET_LIST); do
+   # Now need to call JIRA API to get ticket summary based on those keys in TICKET_LIST
+   SUMMARY=$(curl -s "$Server$i?fields=summary" | jq ".fields.summary")
+   # Summary returned will have "" as the first and last character
+   # Remove first character from summary
+   SUMMARY=${SUMMARY#?}
+   # Remove last character from summary
+   SUMMARY=${SUMMARY%?}
+   echo $i $SUMMARY
+done
